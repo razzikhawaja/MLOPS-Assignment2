c57e4e5330cf06a5822556bf83b044e7b8ea58fd
Razzi Khawaja <i201757@nu.edu.pk>
2024-10-31 19:13:33 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -50,5 +50,5 @@ app.post('/login', (req, res) => {
 
 // Start the server
 app.listen(3001, () => {
-  console.log("Server listening on http://127.0.0.1:3001");
+  console.log("Server listening on http://127.0.0.1:5000");
 });
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        axios.post( 'http://localhost:3001/register', {name, email, password})
+        axios.post( 'http://localhost:5000/register', {name, email, password})
         .then(result => {
             console.log(result);
             if(result.data === "Already registered"){
